6d7a039a7f01048f1ec164d242b209b2f1f49bca
Vincent Puget <puget.vincent@gmail.com>
2016-09-20 13:15:08 +0200

    update Const.swift

--- PhoneList/class/Utils/Const.swift
+++ PhoneList/class/Utils/Const.swift
@@ -13,9 +13,11 @@ struct Const {
     static let DEBUG:Bool = true;
   }
   struct Webservice {
-    static let URL: String = ""
-    static let PHONE_ENDPOINT: String = ""
-    static let PHONE_ENDPOINT_$GET_VERSION: String = "version"
+    static let URL: String = "--"
+    static let PHONE_ENDPOINT: String = "--"
+    static let PHONE_ENDPOINT_$GET_VERSION: String = "--"
+    static let X_JSON_MD5_KEY: String = "--"
+    static let X_JSON_MD5_VALUE: String = "--"
     
     static let PERSON_KEY: String = "persons"
     static let VERSION_KEY: String = "version"
