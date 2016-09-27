3ed770557335d2d8a64840fa3a5a456c4c2a2cd3
Vincent Puget <puget.vincent@gmail.com>
2016-09-27 09:53:33 +0200

    add Const with fake data

---
+++ PhoneList/class/Utils/Const.swift
@@ -0,0 +1,25 @@
+//
+//  Const.swift
+//  PhoneList
+//
+//  Created by Vincent PUGET on 16/09/2016.
+//  Copyright © 2016 Vincent PUGET. All rights reserved.
+//
+
+
+struct Const {
+  struct App {
+    static let NAME:String = "PhoneList";
+    static let DEBUG:Bool = true;
+  }
+  struct Webservice {
+    static let URL: String = ""
+    static let PHONE_ENDPOINT: String = ""
+    static let PHONE_ENDPOINT_$GET_VERSION: String = ""
+    static let X_JSON_MD5_KEY: String = ""
+    static let X_JSON_MD5_VALUE: String = ""
+    
+    static let PERSON_KEY: String = "persons"
+    static let VERSION_KEY: String = "version"
+  }
+}
